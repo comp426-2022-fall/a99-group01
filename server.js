b69a02058aa8d7af1c08e8298c7cef2fe1262dec
niharcv <niharcv@unc.edu>
2022-11-23 01:21:02 -0500

    login and authenticating users done

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ import minimist from 'minimist';
 import express from 'express';
 import path from 'path';
 import {fileURLToPath} from 'url';
+import Database from "better-sqlite3"
+
+const db = new Database('project.db');
+db.pragma('journal_mode = WAL');
+
+
+const sqlInit = `CREATE TABLE users ( id INTEGER PRIMARY KEY AUTO_INCREMENT, user VARCHAR, pass VARCHAR );`
+try {
+    db.exec(sqlInit);
+} catch (error) {
+
+}
+
 
 const args = minimist(process.argv.slice(2));
 const port = args.port || 2000
@@ -29,8 +42,42 @@ app.post('/login', function(req, res) {
     const user = req.body.username;
     const pass = req.body.password;
 
-    res.send(user + pass);
+    const stmt = db.prepare(`SELECT * FROM users WHERE user='${user}' and pass='${pass}';`);
+    let row = stmt.get();
+    if (row === undefined) {
+        res.redirect('/stale_login');
+    } else {
+        res.redirect('/home');
+    }
 });
 
+app.get('/stale_login', function(req, res){
+    res.render('bad_login');
+});
+
+app.get('/home', function(req, res){
+    res.render('home');
+});
+
+app.get('/db', function(req, res){
+    const stmt = db.prepare(`SELECT * FROM users;`);
+    let row = stmt.get();
+
+    if(row === undefined) {
+        res.send('nothing in db');
+    } else {
+        res.send(row);
+    }
+});
+
+app.post('/newacc', function(req, res) {
+    const user = req.body.username;
+    const pass = req.body.password;
+
+    const stmt = `INSERT INTO users (user, pass) VALUES ('${user}', '${pass}');`;
+    db.exec(stmt)
+
+    res.render('new_acc_made');
+});
 
 app.listen(port)
