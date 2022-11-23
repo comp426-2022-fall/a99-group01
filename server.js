5a3eba5b143c671e816d413e0a40ea81ca693f36
niharcv <niharcv@unc.edu>
2022-11-23 03:00:16 -0500

    Everything implemented

--- server.js
+++ server.js
@@ -8,11 +8,16 @@ const db = new Database('project.db');
 db.pragma('journal_mode = WAL');
 
 
-const sqlInit = `CREATE TABLE users ( id INTEGER PRIMARY KEY AUTO_INCREMENT, user VARCHAR, pass VARCHAR );`
+const sqlInit = `CREATE TABLE users ( id INTEGER PRIMARY KEY AUTOINCREMENT, user VARCHAR, pass VARCHAR );`
 try {
     db.exec(sqlInit);
 } catch (error) {
+}
 
+const sqlInit2 = `CREATE TABLE data ( id INTEGER PRIMARY KEY AUTOINCREMENT, user VARCHAR, message VARCHAR, sentiment VARCHAR, date VARCHAR );`
+try {
+    db.exec(sqlInit2);
+} catch (error) {
 }
 
 
@@ -47,6 +52,8 @@ app.post('/login', function(req, res) {
     if (row === undefined) {
         res.redirect('/stale_login');
     } else {
+        req.app.set('user', user);
+        req.app.set('pass', pass);
         res.redirect('/home');
     }
 });
@@ -58,6 +65,41 @@ app.get('/stale_login', function(req, res){
 app.get('/home', function(req, res){
     res.render('home');
 });
+
+app.get('/acc_info', function(req, res){
+    res.render('acc_info', {user: req.app.get('user'), pass: req.app.get('pass')});
+});
+
+app.get('/view_history', function(req, res){
+    const stmt = db.prepare(`SELECT * FROM data WHERE user = '${req.app.get('user')}';`);
+    let all = stmt.all();
+    res.render('view_history', {data: all});
+});
+
+app.get('/new_entry', function(req, res){
+    res.render('new_entry');
+});
+
+app.post('/new_entry', function(req, res){
+    const user = req.app.get('user');
+    const message = req.body.message;
+    const sentiment = req.body.sentiment;
+    const date = req.body.date;
+    const stmt = `INSERT INTO data (user, message, sentiment, date) VALUES ('${user}', '${message}', '${sentiment}', '${date}');`;
+    db.exec(stmt)
+
+    res.render('success_entry');
+
+});
+
+app.post('/delete_acc', function(req, res){
+    const user = req.body.username;
+    const pass = req.body.password;
+    const stmt = `DELETE FROM users WHERE user='${user}' and pass='${pass}';`
+    db.exec(stmt)
+    res.render('acc_deleted');
+});
+
 
 app.get('/db', function(req, res){
     const stmt = db.prepare(`SELECT * FROM users;`);
@@ -74,10 +116,17 @@ app.post('/newacc', function(req, res) {
     const user = req.body.username;
     const pass = req.body.password;
 
-    const stmt = `INSERT INTO users (user, pass) VALUES ('${user}', '${pass}');`;
-    db.exec(stmt)
+    const stmt1 = db.prepare(`SELECT * FROM users WHERE user='${user}'`);
+    let row = stmt1.get();
 
-    res.render('new_acc_made');
+    if (row === undefined) {
+        const stmt = `INSERT INTO users (user, pass) VALUES ('${user}', '${pass}');`;
+        db.exec(stmt)
+        res.render('new_acc_made');
+    } else {
+        res.render('username_exists')
+    }
+    
 });
 
 app.listen(port)
